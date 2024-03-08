8c9b306580048a2772da0611657f4d3c50ddec80
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2024-03-09 03:17:54 +0500

    add java courses questions

---
+++ assets/js/java.js
@@ -0,0 +1,54 @@
+const javaQuestions = [
+    {
+        question: 'In Java, what is the purpose of the ' + '`interface`' + ' keyword?',
+        options: ['To declare a class', 'To create an abstract class', 'To define a protocol or contract for classes', 'To implement multiple inheritance'],
+        correctAnswer: 2
+    },
+    {
+        question: 'What is the role of the ' + '`try-catch`' + ' block in Java?',
+        options: ['To declare variables', 'To handle exceptions', 'To define a loop', 'To create an instance of a class'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Which keyword is used to prevent a method from being overridden in Java?',
+        options: ['final', 'static', 'abstract', 'private'],
+        correctAnswer: 0
+    },
+    {
+        question: 'What is the purpose of the ' + '`instanceof`' + ' operator in Java?',
+        options: ['To check if an object is an instance of a specific class or interface', 'To compare two objects', 'To check if a variable is initialized', 'To perform bitwise operations'],
+        correctAnswer: 0
+    },
+    {
+        question: 'How is memory management handled in Java?',
+        options: ['Automatically by the garbage collector', 'Manually by the programmer using malloc and free', 'Through the use of pointers', 'By declaring variables with the "memory" keyword'],
+        correctAnswer: 0
+    },
+    {
+        question: 'What is the purpose of the ' + '`this`' + ' keyword in a constructor?',
+        options: ['To create a new instance of the class', 'To refer to the current object', 'To access class variables', 'To declare an interface'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Which Java keyword is used to implement method overloading?',
+        options: ['override', 'extends', 'super', 'None of the above'],
+        correctAnswer: 3
+    },
+    {
+        question: 'What is the default value of a local variable in Java?',
+        options: ['0', 'null', 'false', 'Depends on the data type'],
+        correctAnswer: 3
+    },
+    {
+        question: 'In Java, what is the purpose of the ' + '`synchronized`' + ' keyword?',
+        options: ['To prevent multiple threads from executing a block of code simultaneously', 'To define a constant', 'To create an instance of a class', 'To declare an abstract class'],
+        correctAnswer: 0
+    },
+    {
+        question: 'What is the use of the ' + '`StringBuilder`' + ' class in Java?',
+        options: ['To create immutable strings', 'To manipulate strings efficiently', 'To access class variables', 'To define a constant'],
+        correctAnswer: 1
+    }
+];
+
+export default javaQuestions;
