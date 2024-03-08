4546cfbd1707f96a5c0a6ab26da610a4ba6636fe
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2024-03-09 03:19:52 +0500

    add python questions

---
+++ assets/js/python.js
@@ -0,0 +1,54 @@
+const pythonQuestions = [
+    {
+        'question': 'In Python, what is the purpose of the `__init__` method in a class?',
+        'options': ['To create a new instance of the class', 'To initialize class variables', 'To define a constant', 'To access static methods'],
+        'correctAnswer': 1
+    },
+    {
+        'question': 'What is the difference between a list and a tuple in Python?',
+        'options': ['Lists are immutable, and tuples are mutable', 'Lists are ordered, and tuples are unordered', 'Lists are resizable, and tuples have a fixed size', 'There is no difference'],
+        'correctAnswer': 2
+    },
+    {
+        'question': 'In Python, how do you open a file named "example.txt" in read mode?',
+        'options': ['file = open("example.txt", "r")', 'file = open("example.txt", "w")', 'file = open("example.txt", "a")', 'file = read("example.txt")'],
+        'correctAnswer': 0
+    },
+    {
+        'question': 'What does the `yield` keyword do in Python?',
+        'options': ['Exits the function and returns a value', 'Generates a sequence of values lazily', 'Defines a decorator', 'Creates a recursive function'],
+        'correctAnswer': 1
+    },
+    {
+        'question': 'Which of the following is used to handle exceptions in Python?',
+        'options': ['try-except', 'if-else', 'for-in', 'while'],
+        'correctAnswer': 0
+    },
+    {
+        'question': 'What is the purpose of the `lambda` keyword in Python?',
+        'options': ['To create anonymous functions', 'To declare a variable', 'To define a class', 'To access class variables'],
+        'correctAnswer': 0
+    },
+    {
+        'question': 'How do you remove an item from a list in Python?',
+        'options': ['list.remove(item)', 'list.delete(item)', 'list.pop(item)', 'list.discard(item)'],
+        'correctAnswer': 0
+    },
+    {
+        'question': 'What is the result of the expression `3 + 2 * 4` in Python?',
+        'options': ['20', '11', '15', 'Error'],
+        'correctAnswer': 2
+    },
+    {
+        'question': 'Which module in Python provides support for regular expressions?',
+        'options': ['re', 'regex', 'regexp', 'string'],
+        'correctAnswer': 0
+    },
+    {
+        'question': 'What is the purpose of the `with` statement in Python?',
+        'options': ['To define a new context manager', 'To create a loop', 'To handle exceptions', 'To import modules'],
+        'correctAnswer': 0
+    }
+];
+
+export default pythonQuestions;
