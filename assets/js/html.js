4c76138c57af924c2224599213200b17b40d79d5
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2024-03-09 03:24:57 +0500

    create html questions

---
+++ assets/js/html.js
@@ -0,0 +1,56 @@
+
+const cppQuestions = [
+  
+    {
+        question: 'What does HTML stand for?',
+        options: ['Hyper Text Markup Language', 'Highly Typed Machine Learning', 'Hyper Transferable Machine Language', 'Hyperlink and Text Markup Language'],
+        correctAnswer: 0
+    },
+    {
+        question: 'Which HTML tag is used to define an unordered list?',
+        options: ['<ul>', '<ol>', '<li>', '<dl>'],
+        correctAnswer: 0
+    },
+    {
+        question: 'What is the purpose of the HTML "alt" attribute in an image tag?',
+        options: ['To provide alternative text for the image', 'To set the alignment of the image', 'To define the source of the image', 'To create a hyperlink for the image'],
+        correctAnswer: 0
+    },
+    {
+        question: 'In HTML, what does the acronym DOCTYPE stand for?',
+        options: ['Document Type', 'Document Text', 'Document Transition', 'Document Typography'],
+        correctAnswer: 0
+    },
+    {
+        question: 'Which HTML tag is used to create a hyperlink?',
+        options: ['<a>', '<link>', '<href>', '<p>'],
+        correctAnswer: 0
+    },
+    {
+        question: 'What is the purpose of the HTML "meta" tag?',
+        options: ['To define metadata about an HTML document', 'To create a navigation menu', 'To set the font style of text', 'To include external scripts in the document'],
+        correctAnswer: 0
+    },
+    {
+        question: 'Which HTML tag is used to define the structure of an HTML document?',
+        options: ['<head>', '<body>', '<html>', '<title>'],
+        correctAnswer: 2
+    },
+    {
+        question: 'What is the correct HTML for creating a checkbox?',
+        options: ['<input type="checkbox">', '<check>', '<checkbox>', '<input type="check">'],
+        correctAnswer: 0
+    },
+    {
+        question: 'In HTML, what does the acronym CSS stand for?',
+        options: ['Cascading Style Sheet', 'Central Style System', 'Computer Style Syntax', 'Creative Style Sheet'],
+        correctAnswer: 0
+    },
+    {
+        question: 'Which HTML tag is used to define a table?',
+        options: ['<table>', '<tab>', '<tr>', '<tbl>'],
+        correctAnswer: 0
+    }
+];
+
+export default cppQuestions;
